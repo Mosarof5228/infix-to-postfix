736c1927a5440d562cc0006ccb4f418daf4935f4
Md.Mosarof Hossen <mosarofhossen5228@gmail.com>
2025-07-11 09:15:48 +0600

    finally prefix and postfix done

---
+++ fixAndPost.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<stack>
+#include<cctype>
+using namespace std;
+int main(){
+  char myArray[] = {'A','+','(','B','*','C','-','(','D','/','E','^','F',')','*','G',')','*','H'};
+    stack<char>myStack;
+    string postfix ="";
+    int size=sizeof(myArray)/sizeof(myArray[0]);
+    for(int i=0;i<size;i++){
+        char ch=myArray[i];
+        if(isalpha(ch)){
+            postfix=postfix+ch;
+        }
+        else if(ch=='('){
+            myStack.push('(');
+
+        }
+        else if(ch==')'){
+            while (!myStack.empty() && myStack.top()!='('){
+
+                postfix=postfix+myStack.top();
+                myStack.pop();
+            }
+           if(!myStack.empty()){
+            myStack.pop();
+           }
+
+        }
+        else{
+            while(!myStack.empty()){
+                char top=myStack.top();
+
+                bool higherOrLowerPrecedency={
+                     (ch == '+' || ch == '-') && (top == '+' || top == '-' || top == '*' || top == '/' || top == '^') ||
+                    (ch == '*' || ch == '/') && (top == '*' || top == '/' || top == '^') ||
+                    (ch == '^') && (top == '^')
+                };
+                if(higherOrLowerPrecedency){
+                    postfix=postfix+top;
+                    myStack.pop();
+                }
+                else{
+                    break;
+                }
+                
+            }
+            myStack.push(ch);
+        }
+
+    } 
+    while(!myStack.empty()){
+        postfix=postfix+myStack.top();
+        myStack.pop();
+    } 
+    cout<<postfix;
+}
---
+++ infixpostfixdynamic.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+int main(){
+    int arraySize;
+    cout<<"please enter array size: ";
+    char myArray[arraySize];
+    cout<<"please take input for array: ";
+    for(int i=0;i<arraySize;i++){
+        cin>>myArray[i];
+    }
+}
